a18bf1fe3d4eecde399df72cf5b70beb59e2ad27
Matt Felsen <mattfelsen@gmail.com>
2015-10-25 13:03:03 -0400

    ColorStream working, needed to configure stream

--- example/src/FrameListener.h
+++ example/src/FrameListener.h
@@ -16,19 +16,36 @@ class FrameListener : public astra::FrameReadyListener {
 
 public:
 
-	FrameListener() : streamset(nullptr), reader(nullptr) {
-	}
+	FrameListener() :
+		streamset(nullptr),
+		reader(nullptr)
+	{}
 
 	~FrameListener(){
 		astra::Astra::terminate();
 	}
 
 	void setup(){
+		int width = 640;
+		int height = 480;
+		img.allocate(width, height, OF_IMAGE_COLOR);
+
 		astra::Astra::initialize();
 
 		streamset = make_unique<astra::StreamSet>();
 		reader = make_unique<astra::StreamReader>(streamset->create_reader());
-		reader->stream<astra::ColorStream>().start();
+
+		astra::ImageStreamMode colorMode;
+		auto colorStream = reader->stream<astra::ColorStream>();
+
+		colorMode.set_width(width);
+		colorMode.set_height(height);
+		colorMode.set_pixelFormat(astra_pixel_formats::ASTRA_PIXEL_FORMAT_RGB888);
+		colorMode.set_fps(30);
+
+		colorStream.set_mode(colorMode);
+		colorStream.start();
+
 		reader->addListener(*this);
 	}
 
@@ -40,30 +57,6 @@ public:
 		img.draw(0, 0);
 	}
 
-
-	void print_color(astra::ColorFrame& colorFrame){
-		if (colorFrame.is_valid())
-		{
-			int width = colorFrame.resolutionX();
-			int height = colorFrame.resolutionY();
-			int frameIndex = colorFrame.frameIndex();
-
-			astra::RGBPixel* buffer = new astra::RGBPixel[colorFrame.numberOfPixels()];
-			colorFrame.copy_to(buffer);
-
-			size_t index = ((width * (height / 2.0f)) + (width / 2.0f));
-			astra::RGBPixel middle = buffer[index];
-
-			std::cout << "color frameIndex: " << frameIndex
-			<< " r: " << static_cast<int>(middle.r)
-			<< " g: " << static_cast<int>(middle.g)
-			<< " b: " << static_cast<int>(middle.b)
-			<< std::endl;
-
-			delete[] buffer;
-		}
-	}
-
 	virtual void on_frame_ready(astra::StreamReader& reader,
 								astra::Frame& frame) override
 	{
@@ -72,21 +65,10 @@ public:
 		int width = colorFrame.resolutionX();
 		int height = colorFrame.resolutionY();
 
-		img.allocate(width, height, OF_IMAGE_GRAYSCALE);
-        const astra_rgb_pixel_t* colorData = colorFrame.data();
+		const astra::RGBPixel* colorData = colorFrame.data();
 
-		if (colorFrame.is_valid())
-		{
-//			print_color(colorFrame);
-
-			for (int i = 0; i < width * height; i++)
-			{
-				ofColor c;
-				c.r = static_cast<int>(colorData[i].r);
-				c.g = static_cast<int>(colorData[i].g);
-				c.b = static_cast<int>(colorData[i].b);
-				img.setColor(i, c);
-			}
+		if (colorFrame.is_valid()) {
+			colorFrame.copy_to((astra::RGBPixel*) img.getPixels().getData());
 			img.update();
 		}
 	}
